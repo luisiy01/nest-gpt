3abbf34bea0bb4fdede708141559bb79863f770d
luis nunez <l.nunez@blackstone.studio>
2026-04-01 16:26:14 -0600

    editar images

--- src/gpt/use-cases/image-generation.use-case.ts
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -1,5 +1,7 @@
 import OpenAI from "openai";
-import { downloadImageAsPng } from "src/helpers";
+import { downloadBase64ImageAsPng, downloadImageAsPng } from "src/helpers";
+import * as fs from 'fs';
+import * as path from 'path';
 
 interface Options {
     prompt: string;
@@ -10,8 +12,39 @@ interface Options {
 export const imageGenerationUseCase = async (openai: OpenAI, options: Options) => {
     const { prompt, originalImage, maskImage } = options;
 
-    const resp = await openai.images.generate({
+    if (!originalImage || !maskImage) {
+        const resp = await openai.images.generate({
+            model: 'dall-e-3',
+            prompt: prompt,
+            n: 1,
+            size: '1024x1024',
+            quality: 'standard',
+            response_format: 'url',
+        });
+
+        console.log(resp)
+
+        const url = resp.data?.[0]?.url;
+        if (!url) {
+            throw new Error('No image was generated');
+        }
+
+        const fileName = await downloadImageAsPng(url);
+
+        return {
+            url: fileName,
+            openAIUrl: resp.data?.[0]?.url,
+            revised_prompt: resp.data?.[0]?.revised_prompt,
+        };
+    }
+
+    const pngImagePath = await downloadImageAsPng(originalImage);
+    const maskPath = await downloadBase64ImageAsPng(maskImage);
+
+    const resp = await openai.images.edit({
         model: 'dall-e-3',
+        image: fs.createReadStream(pngImagePath),
+        mask: fs.createReadStream(maskPath),
         prompt: prompt,
         n: 1,
         size: '1024x1024',
@@ -19,17 +52,18 @@ export const imageGenerationUseCase = async (openai: OpenAI, options: Options) =
         response_format: 'url',
     });
 
-    console.log(resp)
-
     const url = resp.data?.[0]?.url;
     if (!url) {
         throw new Error('No image was generated');
     }
 
-    const fileName = await downloadImageAsPng(url);
+    const localImagePath = await downloadImageAsPng(url);
+    const fileName = path.basename(localImagePath);
+
+    const publicUrl = `localhost:3000/${fileName}`;
 
     return {
-        url: fileName,
+        url: publicUrl,
         openAIUrl: resp.data?.[0]?.url,
         revised_prompt: resp.data?.[0]?.revised_prompt,
     };
--- src/helpers/download-image-as-png.ts
+++ src/helpers/download-image-as-png.ts
@@ -32,3 +32,27 @@ export const downloadImageAsPng = async (url: string) => {
 
     return path.join(folderPath, imageNamePng);
 }
+
+
+export const downloadBase64ImageAsPng = async (base64Image: string) => {
+
+    // Remover encabezado
+    base64Image = base64Image.split(';base64,').pop() || '';
+    const imageBuffer = Buffer.from(base64Image, 'base64');
+
+    const folderPath = path.resolve('./', './generated/images/');
+    fs.mkdirSync(folderPath, { recursive: true });
+
+    const imageNamePng = `${new Date().getTime()}-64.png`;
+
+    const completePath = path.join(folderPath, imageNamePng);
+
+    // Transformar a RGBA, png // Así lo espera OpenAI
+    await sharp(imageBuffer)
+        .png()
+        .ensureAlpha()
+        .toFile(completePath);
+
+    return completePath;
+
+}
