6f3c22fc086a8894714a63bc2ab8655bd628f544
luis nunez <l.nunez@blackstone.studio>
2026-03-31 23:46:01 -0600

    primer prompt

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { orthographyCheckUseCase } from './use-cases';
 import { OrthographyDto } from './dtos';
+import OpenAI from 'openai';
 
 @Injectable()
 export class GptService {
 
+    private openAI = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+
     async orthographyCheck(orthographyDto: OrthographyDto) {
-        return await orthographyCheckUseCase({
+        return await orthographyCheckUseCase(this.openAI, {
             prompt: orthographyDto.prompt,
         });
     }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,11 +1,30 @@
+import OpenAI from "openai";
+
 interface Options {
     prompt: string;
 }
 
-export const orthographyCheckUseCase = (options: Options) => {
+export const orthographyCheckUseCase = async (openAI: OpenAI, options: Options) => {
 
 
     const { prompt } = options;
 
-    return { prompt: prompt };
+
+    const completion = await openAI.chat.completions.create({
+        model: "gpt-4o-mini",
+        messages: [
+            {
+                role: "system",
+                content: "tu nombre es Pedro Camilo, debes de responder amablemente siempre y dar tu nombre",
+            },
+            {
+                role: "user",
+                content: prompt,
+            },
+        ],
+    });
+
+    console.log(completion);
+
+    return completion.choices[0];
 }
