42de588eaa33421e43d2ede2e93865d75ceaff2b
luis nunez <l.nunez@blackstone.studio>
2026-03-31 23:05:43 -0600

    rutas y cors

---
+++ src/gpt/gpt.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { orthographyCheckUseCase } from './use-cases';
+
+@Injectable()
+export class GptService {
+
+    async orthographyCheck() {
+        return await orthographyCheckUseCase();
+    }
+
+}
---
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -0,0 +1,3 @@
+export const orthographyCheckUseCase = () => {
+    return { hola: 'hola mundo' };
+}
