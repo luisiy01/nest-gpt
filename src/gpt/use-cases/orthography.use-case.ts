9b0b2d1db4be3b24cc03afb4dcc5fbdfdc704179
luis nunez <l.nunez@blackstone.studio>
2026-03-31 23:56:58 -0600

    retornar json

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -39,7 +39,12 @@ export const orthographyCheckUseCase = async (openAI: OpenAI, options: Options) 
         ],
         temperature: 0.3,
         max_tokens: 150,
+        response_format: {
+            type: "json_object",
+        },
     });
 
-    return completion.choices[0];
+    const jsonResp = JSON.parse(completion.choices[0].message.content || '');
+
+    return jsonResp;
 }
